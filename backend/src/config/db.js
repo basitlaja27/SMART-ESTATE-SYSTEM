93319f2c65824ed8e0971e6f41891997fce4a04d
Starboycoded <143321014+Starboycoded@users.noreply.github.com>
2026-03-25 16:15:36 +0100

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "app_user",
-  password: process.env.DB_PASSWORD || "1234",
-  database: process.env.DB_NAME || "smart_estate",
-  port: 3306
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "app_user",
+    password: process.env.DB_PASSWORD || "1234",
+    database: process.env.DB_NAME || "smart_estate",
+    port: process.env.DB_PORT || 3306,
+    ssl: process.env.DB_HOST ? { rejectUnauthorized: true } : undefined
 });
 
 // Test connection
