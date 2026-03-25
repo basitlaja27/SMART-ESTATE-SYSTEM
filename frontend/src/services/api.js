2b3803551bbfce7a14cb5536440a8b62c085b1fa
Starboycoded <143321014+Starboycoded@users.noreply.github.com>
2026-03-25 22:42:03 +0100

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,27 +1,16 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:5000/api",
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default api;
-
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) config.headers.Authorization = Bearer ${token};
   return config;
 });
 
-export default api;
+export default api; 
