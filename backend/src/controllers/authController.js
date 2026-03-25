0d1a09cf81f898b3e06fd2634992b5b172bde77a
Starboycoded <143321014+Starboycoded@users.noreply.github.com>
2026-03-25 23:32:05 +0100

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -6,7 +6,7 @@ const { secret, expiresIn } = require("../config/jwt");
 // Register a new user
 const register = async (req, res) => {
   const { name, email, password, role } = req.body;
-  if (!name  !email  !password) {
+  if (!name || !email || !password) {
     return res.status(400).json({ message: "Name, email, and password are required" });
   }
   const userRole = role  "resident";
